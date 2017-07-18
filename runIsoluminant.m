915c4a4d58d77f965695694256004d38b2db80c8
Ian <ian@aristotle-linux>
2017-07-18 15:31:50 +0800

    make trial structure methods of constants design

--- runIsoluminant.m
+++ runIsoluminant.m
@@ -1,5 +1,7 @@
 function runIsoluminant(ana)
 
+fprintf('\n--->>> runIsoluminant Started!\n');
+
 %===================Initiate out metadata===================
 ana.date = datestr(datetime);
 ana.version = Screen('Version');
@@ -14,19 +16,20 @@ end
 
 %===================Make a name for this run===================
 pf='IsoLum_';
-nameExp = [pf ana.subject];
-c = sprintf(' %i',fix(clock()));
-nameExp = [nameExp c];
-ana.nameExp = regexprep(nameExp,' ','_');
+if ~isempty(ana.subject)
+	nameExp = [pf ana.subject];
+	c = sprintf(' %i',fix(clock()));
+	nameExp = [nameExp c];
+	ana.nameExp = regexprep(nameExp,' ','_');
+else
+	ana.nameExp = 'debug';
+end
 
 cla(ana.plotAxis1);
 cla(ana.plotAxis2);
 
-xxx = 0;
-
 try
 	PsychDefaultSetup(2);
-	xxx = xxx + 1;
 	Screen('Preference', 'SkipSyncTests', 0);
 	%===================open our screen====================
 	sM = screenManager();
@@ -47,14 +50,40 @@ try
 	end
 	sM.backgroundColour = ana.backgroundColor;
 	sM.open; % OPEN THE SCREEN
-	fprintf('\n--->>> %i ISOLUM Opened Screen %i : %s\n', xxx, sM.win, sM.fullName);
+	fprintf('\n--->>> runIsoluminant Opened Screen %i : %s\n', sM.win, sM.fullName);
 	
-	Screen('Preference', 'DefaultFontName','DejaVu Sans');
+	if IsLinux
+		Screen('Preference', 'DefaultFontName','DejaVu Sans');
+	end
 	
 	%============================SET UP VARIABLES=====================================
-	ana.nTrials = abs((sum(ana.colorEnd)-sum(ana.colorStart))/sum(ana.colorStep)) + 1; %
-	ana.onFrames = round((1/ana.frequency) * sM.screenVals.fps) / 2; % video frames for each color
-	fprintf('--->>> ISOLUM # Trials: %i; # Frames Flip: %i; FPS: %i \n',ana.nTrials, ana.onFrames, sM.screenVals.fps);
+	
+	len = 0;
+	for i = 1:3
+		step = (ana.colorEnd(i) - ana.colorStart(i)) / ana.colorStep;
+		r{i} = [ana.colorStart(i) : step : ana.colorEnd(i)]';
+		if length(r{i}) > len; len = length(r{i}); end
+	end
+	for i = 1:length(r)
+		if isempty(r{i})
+			r{i} = zeros(len,1);
+		end
+	end
+	vals = cell(len,1);
+	for i = 1:len
+		vals{i} = [r{1}(i) r{2}(i) r{3}(i)];
+	end
+	
+	seq = stimulusSequence;
+	seq.nVar(1).name = 'colour';
+	seq.nVar(1).stimulus = 1;
+	seq.nVar(1).values = vals;
+	seq.nBlocks = ana.trialNumber;
+	seq.initialise();
+	ana.nTrials = seq.nRuns;
+	%ana.nTrials = abs((sum(ana.colorEnd)-sum(ana.colorStart))/sum(ana.colorStep)) + 1; %
+	ana.onFrames = round(((1/ana.frequency) * sM.screenVals.fps) / 2); % video frames for each color
+	fprintf('--->>> runIsoluminant # Trials: %i; # Frames Flip: %i; FPS: %i \n',seq.nRuns, ana.onFrames, sM.screenVals.fps);
 	WaitSecs('YieldSecs',0.25);
 	diameter = ceil(ana.circleDiameter*sM.ppd);
 	circleRect = [0,0,diameter,diameter];
@@ -62,7 +91,7 @@ try
 	
 	%==============================setup eyelink==========================
 	ana.strictFixation = true;
-	fprintf('--->>> ISOLUM eL setup starting...\n');
+	fprintf('--->>> runIsoluminant eL setup starting...\n');
 	eL = eyelinkManager('IP',[]);
 	%eL.verbose = true;
 	eL.isDummy = ana.isDummy; %use dummy or real eyelink?
@@ -82,16 +111,14 @@ try
 		ana.firstFixTime, ana.firstFixDiameter, ana.strictFixation);
 	initialise(eL, sM); %use sM to pass screen values to eyelink
 	setup(eL); % do setup and calibration
-	fprintf('--->>> ISOLUM eL setup complete: %s\n', eL.fullName);
+	fprintf('--->>> runIsoluminant eL setup complete: %s\n', eL.fullName);
 	WaitSecs('YieldSecs',0.5);
 	getSample(eL); %make sure everything is in memory etc.
-	eL.verbose = true;
 	
 	% initialise our trial variables
 	tL = timeLogger();
 	tL.screenLog.beforeDisplay = GetSecs();
 	tL.screenLog.stimTime(1) = 1;
-	iii = 1;
 	powerValues = [];
 	breakLoop = false;
 	ana.trial = struct();
@@ -99,14 +126,14 @@ try
 	halfisi = sM.screenVals.halfisi;
 	Priority(MaxPriority(sM.win));
 	
-	while ~breakLoop
+	while seq.thisRun <= seq.nRuns && ~breakLoop
 		%=========================MAINTAIN INITIAL FIXATION==========================
-		fprintf('===>>> ISOLUM START Trial = %i\n', iii);eL.verbose = false;
+		fprintf('===>>> runIsoluminant START Trial = %i / %i\n', seq.thisRun, seq.nRuns);
 		resetFixation(eL);
 		trackerClearScreen(eL);
 		trackerDrawFixation(eL); %draw fixation window on eyelink computer
 		edfMessage(eL,'V_RT MESSAGE END_FIX END_RT');  %this 3 lines set the trial info for the eyelink
-		edfMessage(eL,['TRIALID ' num2str(iii)]);  %obj.getTaskIndex gives us which trial we're at
+		edfMessage(eL,['TRIALID ' num2str(seq.thisRun)]);  %obj.getTaskIndex gives us which trial we're at
 		startRecording(eL);
 		statusMessage(eL,'INITIATE FIXATION...');
 		fixated = '';
@@ -122,29 +149,29 @@ try
 				rchar = KbName(keyCode); if iscell(rchar);rchar=rchar{1};end
 				switch lower(rchar)
 					case {'c'}
-						fprintf('===>>> ISOLUM recalibrate pressed\n');
+						fprintf('===>>> runIsoluminant recalibrate pressed\n');
 						fixated = 'breakfix';
 						stopRecording(eL);
 						setOffline(eL);
 						trackerSetup(eL);
 						WaitSecs('YieldSecs',2);
 					case {'d'}
-						fprintf('===>>> ISOLUM drift correct pressed\n');
+						fprintf('===>>> runIsoluminant drift correct pressed\n');
 						fixated = 'breakfix';
 						stopRecording(eL);
 						driftCorrection(eL);
 						WaitSecs('YieldSecs',2);
 					case {'escape'}
-						fprintf('===>>> ISOLUM escape pressed\n');
+						fprintf('===>>> runIsoluminant escape pressed\n');
 						fixated = 'breakfix';
 						breakLoop = true;
 				end
 			end
-			WaitSecs('YieldSecs',0.0016);
+			WaitSecs('YieldSecs', sM.screenVals.ifi);
 		end
 		ListenChar(0);
 		if strcmpi(fixated,'breakfix')
-			fprintf('===>>> BROKE INITIATE FIXATION Trial = %i\n', iii);
+			fprintf('===>>> BROKE INITIATE FIXATION Trial = %i\n', seq.thisRun);
 			statusMessage(eL,'Subject Broke Initial Fixation!');
 			edfMessage(eL,'MSG:BreakInitialFix');
 			resetFixation(eL);
@@ -154,22 +181,15 @@ try
 			continue
 		end
 		
-		%if we lost fixation then
-		if ~strcmpi(fixated,'fix')
-			fprintf('===>>> BROKE INITIATE: will retry...\n');
-			continue
-		end
-		
 		%=========================Our actual stimulus drawing loop==========================
 		edfMessage(eL,'END_FIX');
 		statusMessage(eL,'Show Stimulus...');
 		
 		i=1;
-		toggle = 0;
 		ii = 1;
 		toggle = 0;
 		thisPupil = [];
-		modColor = ana.colorStart + (ana.colorStep .* (iii-1));
+		modColor = seq.outValues{seq.thisRun}{1};
 		modColor(modColor < 0) = 0; modColor(modColor > 1) = 1;
 		fixedColor = ana.colorFixed;
 		backColor = modColor;
@@ -177,11 +197,11 @@ try
 		fprintf('===>>> modColor=%s | fixColor=%s\n',num2str(modColor),num2str(fixedColor));
 		edfMessage(eL,['MSG:modColor=' num2str(modColor)]);
 		
-		ana.trial(iii).n = iii;
-		ana.trial(iii).mColor = modColor;
-		ana.trial(iii).fColor = fixedColor;
-		ana.trial(iii).pupil = [];
-		ana.trial(iii).frameN = [];
+		ana.trial(seq.thisRun).n = seq.thisRun;
+		ana.trial(seq.thisRun).mColor = modColor;
+		ana.trial(seq.thisRun).fColor = fixedColor;
+		ana.trial(seq.thisRun).pupil = [];
+		ana.trial(seq.thisRun).frameN = [];
 		
 		tStart = GetSecs; vbl = tStart;if isempty(tL.vbl);tL.vbl(1) = tStart;tL.startTime = tStart; end
 		
@@ -190,7 +210,7 @@ try
 		while GetSecs < tStart + ana.trialDuration
 			if i > ana.onFrames
 				toggle = mod(toggle+1,2); %switch the toggle 0 or 1
-				ana.trial(iii).frameN = [ana.trial(iii).frameN i];
+				ana.trial(seq.thisRun).frameN = [ana.trial(seq.thisRun).frameN i];
 				i = 1; %reset out counter
 				if toggle
 					backColor = fixedColor; centerColor = modColor;
@@ -213,7 +233,7 @@ try
 			ii = ii + 1;
 			if ~isFixated(eL)
 				fixated = 'breakfix';
-				break %break the for loop
+				break %break the while loop
 			end
 		end
 		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -222,31 +242,30 @@ try
 		sM.drawBackground();
 		tEnd=Screen('Flip',sM.win);
 		
-		ana.trial(iii).pupil = thisPupil;
-		ana.trial(iii).totalFrames = ii-1;
+		ana.trial(seq.thisRun).pupil = thisPupil;
+		ana.trial(seq.thisRun).totalFrames = ii-1;
 		
 		% check if we lost fixation
 		if ~strcmpi(fixated,'fix')
-			fprintf('===>>> BROKE FIXATION Trial = %i (%i secs)\n\n', iii, tEnd-tStart);
+			fprintf('===>>> BROKE FIXATION Trial = %i (%i secs)\n\n', seq.thisRun, tEnd-tStart);
 			statusMessage(eL,'Subject Broke Fixation!');
 			edfMessage(eL,'TRIAL_RESULT -1');
 			edfMessage(eL,'MSG:BreakFix');
 			resetFixation(eL);
 			stopRecording(eL);
 			setOffline(eL);
-			continue
 		else
-			fprintf('===>>> SUCCESS: Trial = %i (%i secs)\n\n', iii, tEnd-tStart);
-			ana.trial(iii).success = true;
+			fprintf('===>>> SUCCESS: Trial = %i (%i secs)\n\n', seq.thisRun, tEnd-tStart);
+			ana.trial(seq.thisRun).success = true;
 			stopRecording(eL);
 			edfMessage(eL,'TRIAL_RESULT 1');
 			setOffline(eL);
-			updatePlot(iii);
-			iii = iii+1;
+			updatePlot(seq.thisRun);
+			updateTask(seq,true); %updates our current run number
+			iii = seq.thisRun;
 		end
 		
 		ListenChar(2);
-
 		while GetSecs < tEnd + 0.5
 			[keyIsDown, ~, keyCode] = KbCheck(-1);
 			if keyIsDown == 1
@@ -262,20 +281,22 @@ try
 						driftCorrection(eL);
 						WaitSecs('YieldSecs',2);
 					case {'escape'}
+						trackerClearScreen(eL);
+						stopRecording(eL);
+						setOffline(eL);
 						breakLoop = true;
 				end
 			end
-			WaitSecs('YieldSecs',0.015);
+			WaitSecs('YieldSecs',sM.screenVals.ifi);
 		end
 		ListenChar(0);
-		
-		if iii > ana.nTrials; breakLoop = true; end
 		
 	end % while ~breakLoop
 	
 	%===============================Clean up============================
-	Screen('DrawText', sM.win, 'FINISHED!!!')
-	Screen('Flip',sM.win)
+	fprintf('===>>> runIsoluminant Finished Trials: %i\n',seq.nRuns);
+	Screen('DrawText', sM.win, '===>>> FINISHED!!!');
+	Screen('Flip',sM.win);
 	WaitSecs('YieldSecs', 1);
 	close(sM); breakLoop = true;
 	ListenChar(0);ShowCursor;Priority(0);
@@ -283,14 +304,22 @@ try
 	if exist(ana.ResultDir,'dir') > 0
 		cd(ana.ResultDir);
 	end
+	trackerClearScreen(eL);
+	stopRecording(eL);
+	setOffline(eL);
 	close(eL);
-	ana.plotAxis1 = [];
-	ana.plotAxis2 = [];
-	fprintf('==>> SAVE %s, to: %s\n', ana.nameExp, pwd);
-	save([ana.nameExp '.mat'],'ana','eL', 'sM', 'tL');
-	
+	if ~isempty(ana.nameExp) || ~strcmpi(ana.nameExp,'debug')
+		ana.plotAxis1 = [];
+		ana.plotAxis2 = [];
+		fprintf('==>> SAVE %s, to: %s\n', ana.nameExp, pwd);
+		save([ana.nameExp '.mat'],'ana', 'seq', 'eL', 'sM', 'tL');
+	end
+	tL.printRunLog;
+	clear ana seq eL sM tL
+
 catch ME
-	close(sM);
+	if exist('eL','var'); close(eL); end
+	if exist('sM','var'); close(sM); end
 	ListenChar(0);ShowCursor;Priority(0);Screen('CloseAll');
 	getReport(ME)
 end
